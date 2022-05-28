facbcb0aba61c958eacae6c716b5360f6c4049a6
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2022-05-28 11:53:38 +0800

    Add login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import {MaterialModule} from './shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    SharedModule,
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../shared/material.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [CommonModule, SharedModule, MaterialModule],
+  exports: [LoginComponent]
+})
+export class CoreModule { }
